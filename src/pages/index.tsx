13fc628ac90ddf8c9777e86369e7fafca5328801
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-02-15 07:37:00 -0500

    Create title with contact links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,6 @@ import Title from "@/components/Title"
 const Home: React.FC<PageProps> = () => (
   <main className="container">
     <Title />
-    <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-    <p>
-      Follow me on Twitter (
-      <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-    </p>
   </main>
 )
 
