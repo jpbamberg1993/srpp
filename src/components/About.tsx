8593fc3ba62bd11c2e5e544305f6f2248d87805a
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-02-26 08:09:50 -0500

    Create about section layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 
+import styles from '@/styles/about.module.css'
 import placeholderImage from '@/images/placeholder.svg'
 
 const About: React.FC = () => (
-  <section>
+  <section className={styles.aboutContainer}>
     <div>
       <p>
         We provide preservation services to mortgage brokers, financial institutions and real estate developers with a focus on special assets.
       </p>
-      <img src={placeholderImage} alt='image of business people' />
+      <img src={placeholderImage} alt='image of a man loading a truck' />
     </div>
     <div>
       <p>
         We have been serving Palm Beach, Broward, and Miami Dade counties since 2010.
       </p>
-      <img src={placeholderImage} alt='image of man in truck' />
+      <img src={placeholderImage} alt='image of man driving a truck' />
     </div>
     <div>
       <p>
         All work is guaranteed with timely project updates, documentation and photos.
       </p>
-      <img src={placeholderImage} alt='image of man in truck' />
+      <img src={placeholderImage} alt='an image of a man operating a bobcat grapple bucket' />
     </div>
   </section>
 )
---
+++ src/styles/about.module.css.d.ts
@@ -0,0 +1,1 @@
+export const aboutContainer: string;
