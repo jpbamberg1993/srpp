959d19a7bf2c17dfb06636ab178c3b43b610b3dd
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-02-27 08:47:53 -0500

    Define image type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import { PageProps, graphql } from 'gatsby'
+import { FluidObject } from 'gatsby-image'
 
 import Layout from '@/components/Layout'
 import Contact from '@/components/Contact'
 import Banner from '@/components/Banner'
 import About from '@/components/About'
 
-const Home: React.FC<PageProps> = ({ data }) => (
+type IndexQueryProps = {
+  trucksAtDump: {
+    childImageSharp: {
+      fluid: FluidObject
+    }
+  }
+}
+type IndexPageProps = PageProps<IndexQueryProps>
+
+const Home: React.FC<IndexPageProps> = (props: IndexPageProps) => (
   <Layout>
     <Contact />
-    <Banner bgImage={data.file.childImageSharp.fluid} />
+    <Banner bgImage={props.data.trucksAtDump.childImageSharp.fluid} />
     <About />
   </Layout>
 )
 
 export const query = graphql`
   query {
-    file(relativePath: { eq: "trucks-at-dump.jpg" }) {
+    tucksAtDump: file(relativePath: { eq: "trucks-at-dump.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_noBase64
