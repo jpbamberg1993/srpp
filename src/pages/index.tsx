b048fc1dfdd525e04ea30b08eadc8d6c579bd7af
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-02-27 09:42:49 -0500

    Name image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,25 +8,26 @@ import Banner from '@/components/Banner'
 import About from '@/components/About'
 
 type IndexQueryProps = {
-  trucksAtDump: {
+  tucksatdump: {
     childImageSharp: {
       fluid: FluidObject
     }
   }
 }
+
 type IndexPageProps = PageProps<IndexQueryProps>
 
 const Home: React.FC<IndexPageProps> = (props: IndexPageProps) => (
   <Layout>
     <Contact />
-    <Banner bgImage={props.data.trucksAtDump.childImageSharp.fluid} />
+    <Banner bgImage={props.data.tucksatdump.childImageSharp.fluid} />
     <About />
   </Layout>
 )
 
 export const query = graphql`
   query {
-    tucksAtDump: file(relativePath: { eq: "trucks-at-dump.jpg" }) {
+    tucksatdump: file(relativePath: { eq: "trucks-at-dump.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_noBase64
