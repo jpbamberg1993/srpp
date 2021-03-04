f9e631d2b4dde7a7bd9d1c0b0b799d4f6cb629d3
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-03-04 10:00:48 -0500

    Get before and after slider to function

--- src/components/BeforeAfterSlider.tsx
+++ src/components/BeforeAfterSlider.tsx
@@ -2,13 +2,41 @@ import React from 'react'
 
 import styles from '../styles/before-after-slider.module.css'
 
-const BeforeAfterSlider: React.FC = () => (
-  <section className={styles.container}>
-    <div className={`${styles.img} ${styles.backgroundImg}`}></div>
-    <div className={`${styles.img} ${styles.forgroundImg}`}></div>
-    {/* <input type="range" min="1" max="100" value="50" className="slider" name="slider" id="slider" />
-    <div className="slider-button"></div> */}
-  </section>
-)
+type State = { sliderPosition: string }
+class BeforeAfterSlider extends React.Component<{}, State> {
+  constructor(props) {
+    super(props)
+    this.state = { sliderPosition: '50' }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({ sliderPosition: event.target.value })
+  }
+
+  render() {
+    const sliderPositionWidth: string = this.state.sliderPosition + '%'
+    return (
+        <section className={styles.container}>
+          <div className={`${styles.img} ${styles.backgroundImg}`}></div>
+          <div
+            className={`${styles.img} ${styles.forgroundImg}`}
+            style={{ width: sliderPositionWidth }}
+          ></div>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            value={this.state.sliderPosition}
+            className={styles.slider}
+            name="slider"
+            id="slider"
+            onChange={this.handleChange} />
+          {/* <div className="slider-button"></div> */}
+        </section>
+    )
+  }  
+}
 
 export default BeforeAfterSlider
--- src/styles/before-after-slider.module.css.d.ts
+++ src/styles/before-after-slider.module.css.d.ts
@@ -1,4 +1,6 @@
+export const body: string;
 export const container: string;
 export const img: string;
 export const backgroundImg: string;
 export const forgroundImg: string;
+export const slider: string;
