b9686d69be66c0e367c77dce3d7523ccf693cd81
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-03-06 08:36:09 -0500

    Include animated svgs in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,27 +1,29 @@
 import React from 'react'
 
 import styles from '@/styles/about.module.css'
-import placeholderImage from '@/images/placeholder.svg'
+import drivingBobcat from '@/images/bobcat-animated.svg'
+import drivingPickupTruck from '@/images/driving-pickup-truck-animated.svg'
+import manLoadingTruck from '@/images/trash-man-truck-animated.svg'
 
 const About: React.FC = () => (
   <section className={styles.aboutContainer}>
-    <div>
+    <div className={styles.subSection}>
       <p>
         We provide preservation services to mortgage brokers, financial institutions and real estate developers with a focus on special assets.
       </p>
-      <img src={placeholderImage} alt='image of a man loading a truck' />
+      <img src={manLoadingTruck} alt='image of a man loading a truck' />
     </div>
     <div>
       <p>
         We have been serving Palm Beach, Broward, and Miami Dade counties since 2010.
       </p>
-      <img src={placeholderImage} alt='image of man driving a truck' />
+      <img src={drivingPickupTruck} alt='image of man driving a truck' />
     </div>
     <div>
       <p>
         All work is guaranteed with timely project updates, documentation and photos.
       </p>
-      <img src={placeholderImage} alt='an image of a man operating a bobcat grapple bucket' />
+      <img src={drivingBobcat} alt='an image of a man operating a bobcat grapple bucket' />
     </div>
   </section>
 )
--- src/styles/about.module.css.d.ts
+++ src/styles/about.module.css.d.ts
@@ -1,1 +1,2 @@
 export const aboutContainer: string;
+export const subSection: string;
