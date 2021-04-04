58e9b4271f6766151b6a8024d854569891f06ecf
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-04-04 17:33:00 -0400

    Test honeybot with data

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,36 +2,58 @@ import React, { useState } from 'react'
 import 'react-phone-number-input/style.css'
 import { ContactFormElement } from '../styles/contact-form-element';
 
-const ContactForm: React.FC = () => {
-  return (
-    <ContactFormElement method="post" name="contact" data-netlify="true" netlify-honeypot="bot-field">
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: null,
+      email: null,
+      phoneNumber: null,
+      subject: null,
+      message: null,
+      errors: {
+        name: '',
+        email: '',
+        phoneNumber: '',
+        subject: '',
+        message: '',
+      }
+    }
+  }
 
-      <label>
-        Name
-        <input type="text" name="name" id="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" id="email" />
-      </label>
-      <label>
-        Phone number
-        <input type="tel" name="phone number" id="phone-number" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" id="subject" />
-      </label>
-      <label>
-        Message
-        <textarea name="message" id="message" rows={5} />
-      </label>
-      <button type="submit">Send</button>
-      <input type="reset" value="Clear" />
-    </ContactFormElement>
-  )
+  render() {
+    return (
+      <ContactFormElement method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <input type="hidden" name="bot-field" />
+        </p>
+
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" id="email" />
+        </label>
+        <label>
+          Phone number
+          <input type="tel" name="phoneNumber" id="phone-number" />
+        </label>
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label>
+          Message
+          <textarea name="message" id="message" rows={5} />
+        </label>
+        <button type="submit">Send</button>
+        <input type="reset" value="Clear" />
+      </ContactFormElement>
+    )
+  }
 }
 
 export default ContactForm
