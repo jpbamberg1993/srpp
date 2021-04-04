3799398832714c86950560a53171558f071de05b
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-04-04 15:40:02 -0400

    Re order attributes so they make more sense

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -6,7 +6,7 @@ const ContactForm: React.FC = () => {
   const [value, setValue] = useState()
 
   return (
-    <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+    <form method="post" name="contact" data-netlify="true" netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
 
