eadd75afffd92356af7442f5097bc862674ce3c8
J. Paul Bamberg <jpbamberg1993@gmail.com>
2021-04-04 16:09:47 -0400

    Debug bot field

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,7 +4,7 @@ import { ContactFormElement } from '../styles/contact-form-element';
 
 const ContactForm: React.FC = () => {
   return (
-    <ContactFormElement method="post" name="contact" data-netlify="true" netlify-honeypot="bot-field">
+    <form method="post" name="contact" data-netlify="true" netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
 
@@ -16,10 +16,10 @@ const ContactForm: React.FC = () => {
         Email
         <input type="email" name="email" id="email" />
       </label>
-      {/* <label>
+      <label>
         Phone number
         <input type="tel" name="phone number" id="phone-number" />
-      </label> */}
+      </label>
       <label>
         Subject
         <input type="text" name="subject" id="subject" />
@@ -30,7 +30,7 @@ const ContactForm: React.FC = () => {
       </label>
       <button type="submit">Send</button>
       <input type="reset" value="Clear" />
-    </ContactFormElement>
+    </form>
   )
 }
 
